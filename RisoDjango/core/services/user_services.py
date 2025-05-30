9fa403990767ac625130e4f9820fcf0402cda65e
Casemiro <casemiro.seneme@fatec.sp.gov.br>
2025-05-30 07:59:13 -0300

    Refactor database access in services to use MongoDBConnection for improved consistency and encapsulation

--- RisoDjango/core/services/user_services.py
+++ RisoDjango/core/services/user_services.py
@@ -1,26 +1,34 @@
 import bcrypt
-from .db_connection import get_db
+from .db_connection import MongoDBConnection
+import os
+
+def get_db():
+    db_name = os.environ.get("MONGO_DB_NAME", "riso")
+    mongo = MongoDBConnection(db_name=db_name)
+    return mongo.get_db()
+
+##Talvez seja desnecessário essa collection de usuário pois foi adicionado um model form para usuario
 
 def verify_user(username):
-    db = get_db()
-    user = db["users"].find_one({"username": username})
+    
+    user = get_db()["users"].find_one({"username": username})
     return user is not None 
 
 def create_user(username, password):
-    db = get_db()
+    
     if not verify_user(username):
         hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         user = {
             "username": username,
             "password": hashed
         }
-        db["users"].insert_one(user)
+        get_db()["users"].insert_one(user)
         return True
     return False
 
 def auth_user(username, password):
-    db = get_db()
-    user = db["users"].find_one({"username": username})
+    
+    user = get_db()["users"].find_one({"username": username})
     if user and bcrypt.checkpw(password.encode('utf-8'), user["password"]):
         return True
     return False
@@ -35,29 +43,29 @@ def auth_session(session):
     
 
 def get_user(username):
-    db = get_db()
-    user = db["users"].find_one({"username": username})
+    
+    user = get_db()["users"].find_one({"username": username})
     return user if user else None
 
 def update_user(username, new_data):
-    db = get_db()
+    
 
     if "password" in new_data:
         new_data["password"] = bcrypt.hashpw(new_data["password"].encode('utf-8'), bcrypt.gensalt())
 
-    result = db["users"].update_one({"username": username}, {"$set": new_data})
+    result = get_db()["users"].update_one({"username": username}, {"$set": new_data})
     return result.modified_count > 0
 
 def delete_user(username):
-    db = get_db()
-    result = db["users"].delete_one({"username": username})
+    
+    result = get_db()["users"].delete_one({"username": username})
     return result.deleted_count > 0
 
 def show_users():
-    db = get_db()
-    usuarios = db["users"].find({}, {"password": 0})  # Oculta senhas
+    
+    usuarios = get_db()["users"].find({}, {"password": 0})  # Oculta senhas
     return list(usuarios)
 
 def count_users():
-    db = get_db()
-    return db["users"].count_documents({})
+    
+    return get_db()["users"].count_documents({})
